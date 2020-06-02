bebef476af1088dbb0736bd1d8c7d54e5151f5bd
Socrates-maker <sekpaliguidime@gmail.com>
2020-06-02 22:40:38 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -6,7 +6,12 @@ def main():
     for i in range(0,dice_roll):
         roll=random.randint(1,6)    
         dice_sum+=roll
-        print(f'You rolled a die {roll}')
+        if roll==1:
+            print(f'you rolled a {roll} !critical fail')
+        elif roll==6:
+            print(f'you rolled a {roll} !critical succes')
+        else:
+            print(f'you rolled {roll}')
     
     print(f'you have rolled a total of {dice_sum}')
 
